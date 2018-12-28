cd1e57548235c5db632ca73e2868d79e73554bc8
tanxiaozhang <1040769630@qq.com>
2018-12-28 11:17:03 +0800

    delete useless code

---
+++ src/main/java/com/wux/rcb/elf/biz/Task/DemoTask.java
@@ -0,0 +1,23 @@
+package com.wux.rcb.elf.biz.Task;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 外汇牌价定时任务
+ * */
+@Component
+@EnableScheduling
+public class DemoTask {
+
+//    private Logger logger = LoggerFactory.getLogger(DemoTask.class);
+//
+//    //测试记录
+//    //@Scheduled(fixedRate = 20 * 1000)
+//    public void taskTestExchangeRatePrice() {
+//    }
+}
