0b84f9d3c5825a0a2d0df096922c7e4f6d87612c
tanxiaozhang <1040769630@qq.com>
2020-04-15 16:17:20 +0800

    增加含有des加密的常用工具类

---
+++ src/main/java/com/wux/rcb/elf/util/CommonUtil.java
@@ -0,0 +1,116 @@
+package com.wux.rcb.elf.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import java.security.SecureRandom;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @Desc常用工具类
+ * @Author Tan
+ * @Date 2019/04/18
+ * */
+public class CommonUtil {
+
+    public static final byte[] DES_KEY = { 21, 1, -110, 82, -32, -85, -128, -65 };
+
+    /**去除字符串内的特殊字符*/
+    public static String formatBlank(String sourceStr){
+        String targetStr = "";
+        if(sourceStr != null){
+            Pattern p = Pattern.compile("\\s*|\t|\n|\r");
+            Matcher m = p.matcher(sourceStr);
+            targetStr = m.replaceAll("");
+        }
+        return targetStr;
+    }
+
+    /**身份证号码校验*/
+    public static boolean isIDNumber(String IDNumber) {
+        if (StringUtils.isBlank(IDNumber)) {
+            return false;
+        }
+        String regularExpression = "(^[1-9]\\d{5}(18|19|20)\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx]$)|" + "(^[1-9]\\d{5}\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3}$)";
+        boolean matches = IDNumber.matches(regularExpression);
+        if (matches) {
+            if (IDNumber.length() == 18) {
+                try {
+                    char[] charArray = IDNumber.toCharArray();
+                    //前十七位加权因子
+                    int[] idCardWi = {7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2};
+                    //这是除以11后，可能产生的11位余数对应的验证码
+                    String[] idCardY = {"1", "0", "X", "9", "8", "7", "6", "5", "4", "3", "2"};
+                    int sum = 0;
+                    for (int i = 0; i < idCardWi.length; i++) {
+                        int current = Integer.parseInt(String.valueOf(charArray[i]));
+                        int count = current * idCardWi[i];
+                        sum += count;
+                    }
+                    char idCardLast = charArray[17];
+                    int idCardMod = sum % 11;
+                    if (idCardY[idCardMod].toUpperCase().equals(String.valueOf(idCardLast).toUpperCase())) {
+                        return true;
+                    } else {
+                        System.out.println("身份证最后一位:" + String.valueOf(idCardLast).toUpperCase() + "错误,正确的应该是:" + idCardY[idCardMod].toUpperCase());
+                        return false;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.out.println("异常:" + IDNumber);
+                    return false;
+                }
+            }
+        }
+        return matches;
+    }
+
+    /**
+     *  EDS的加密解密代码
+     */
+    @SuppressWarnings("restriction")
+    public static String encryptBasedDes(String data) {
+        String encryptedData = null;
+        try {
+            // DES算法要求有一个可信任的随机数源
+            SecureRandom sr = new SecureRandom();
+            DESKeySpec deskey = new DESKeySpec(DES_KEY);
+            // 创建一个密匙工厂，然后用它把DESKeySpec转换成一个SecretKey对象
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+            SecretKey key = keyFactory.generateSecret(deskey);
+            // 加密对象
+            Cipher cipher = Cipher.getInstance("DES");
+            cipher.init(Cipher.ENCRYPT_MODE, key, sr);
+            // 加密，并把字节数组编码成字符串
+            encryptedData = new sun.misc.BASE64Encoder().encode(cipher.doFinal(data.getBytes()));
+        } catch (Exception e) {
+            // log.error("加密错误，错误信息：", e);
+            throw new RuntimeException("加密错误，错误信息：", e);
+        }
+        return encryptedData;
+    }
+    @SuppressWarnings("restriction")
+    public static String decryptBasedDes(String cryptData) {
+        String decryptedData = null;
+        try {
+            // DES算法要求有一个可信任的随机数源
+            SecureRandom sr = new SecureRandom();
+            DESKeySpec deskey = new DESKeySpec(DES_KEY);
+            // 创建一个密匙工厂，然后用它把DESKeySpec转换成一个SecretKey对象
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+            SecretKey key = keyFactory.generateSecret(deskey);
+            // 解密对象
+            Cipher cipher = Cipher.getInstance("DES");
+            cipher.init(Cipher.DECRYPT_MODE, key, sr);
+            // 把字符串进行解码，解码为为字节数组，并解密
+            decryptedData = new String(cipher.doFinal(new sun.misc.BASE64Decoder().decodeBuffer(cryptData)));
+        } catch (Exception e) {
+            throw new RuntimeException("解密错误，错误信息：", e);
+        }
+        return decryptedData;
+    }
+}
